0580f20fcef51c4187240fc9bd5af4d5588d128b
IDFCSGFY <idfcsgfy@gmail.com>
2025-05-05 20:10:56 +0700

    changeloges moved to match their intended location as stated in master-log

--- src/main/java/ru/skypro/homework/entity/CommentEntity.java
+++ src/main/java/ru/skypro/homework/entity/CommentEntity.java
@@ -26,4 +26,55 @@ public class CommentEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "ad_id", nullable = false)
     private AdEntity ad;
+
+    public CommentEntity() {
+    }
+
+    public CommentEntity(Integer id, String text, Instant createdAt, UserEntity author, AdEntity ad) {
+        this.id = id;
+        this.text = text;
+        this.createdAt = createdAt;
+        this.author = author;
+        this.ad = ad;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public UserEntity getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(UserEntity author) {
+        this.author = author;
+    }
+
+    public AdEntity getAd() {
+        return ad;
+    }
+
+    public void setAd(AdEntity ad) {
+        this.ad = ad;
+    }
 }
